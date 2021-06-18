27cd72c8192ccf07be904236b5374561d27c2b5e
kaziabida <82897223+kaziabida@users.noreply.github.com>
2021-06-18 23:49:26 +0600

    Update dbcon.php

---
+++ dbcon.php
@@ -0,0 +1,15 @@
+<?php
+
+   $con = mysqli_connect('localhost','root','','sms');
+
+   if($con == false)
+   {
+
+   	 echo "connection is not done";
+   }
+   else
+   {
+
+   	echo "connection ok";
+   }
+?>
